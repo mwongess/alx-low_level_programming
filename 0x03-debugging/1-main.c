4b576fa7f85fe7a5078a7703210a22f428b7e10b
cm-amos <amosmwongelah@gmail.com>
2021-09-02 07:38:39 -0700

    Fix code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-//        int i;
+        int i;
 
         printf("Infinite loop incoming :(\n");
- 
-//        i = 0;
-    
-//      while (i < 10)
-//       {
-//               putchar(i);
-//       }
-  
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
