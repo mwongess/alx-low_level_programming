1f9c28289ecd2930019ce975829d06aaa172432f
cm-amos <amosmwongelah@gmail.com>
2021-10-15 03:19:09 +0300

    Convert a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,4 @@
-/*
- * Auth: Brennan D Baraban
- * File: 0-binary_to_uint.c
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
